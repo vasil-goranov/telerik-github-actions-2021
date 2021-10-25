8cd1a02cb55e47ca78e7c698a78e0f2feadafb7d
Vasil Goranov <vasil.goranov@gmail.com>
2021-10-25 21:12:13 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -29,7 +29,7 @@ def factorial(n):
     """
     time.sleep(.1)
     factorial = 1
-    for i in range (1, n+1):
+    for i in range(1, n+1):
         factorial = factorial * i
     return factorial
 
